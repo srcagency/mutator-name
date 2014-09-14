2ecaac1e2ba96f8438c3053676c9665ed353b9f8
Thomas Jensen <thomas@src.agency>
2014-09-14 13:17:19 +0200

    tests: add a few

--- test.js
+++ test.js
@@ -6,18 +6,24 @@ var m = require('./');
 test(function( t ) {
 	t.equal(m.type('getName'), 'get');
 	t.equal(m.type('setName'), 'set');
-	t.equal(m.type('test'), false);
+	t.notOk(m.type('test'));
 
 	t.equal(m.getter('getName'), true);
 	t.equal(m.getter('setName'), false);
+	t.equal(m.getter('gettingStuff'), false);
+	t.equal(m.getter('other'), false);
 
 	t.equal(m.setter('setName'), true);
 	t.equal(m.setter('getName'), false);
+	t.equal(m.setter('settingStuff'), false);
+	t.equal(m.setter('other'), false);
 
 	t.equal(m('getName'), true);
 	t.equal(m('setAge'), true);
-	t.equal(m('setupConnection'), false);
+	t.equal(m('setupProp'), false);
+	t.equal(m('gettingProp'), false);
 	t.equal(m('get'), false);
+	t.equal(m('set'), false);
 	t.equal(m('getting'), false);
 
 	t.equal(m.getterFrom('name'), 'getName');
